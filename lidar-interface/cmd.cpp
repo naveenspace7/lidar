e64c78a6c0b48e51c1d041931fec6bf00fcdd1f2
naveen chakravarthy <naveen.j_94@outlook.com>
2020-08-09 18:59:13 +0200

    Add scan functions

--- lidar-interface/cmd.cpp
+++ lidar-interface/cmd.cpp
@@ -1,16 +1,52 @@
-// #include <cstdint>
-// #include <iostream>
-
 #include "cmd.hpp"
 #include "interface.hpp"
 
-void SRNR::execute()
-{
-    // std::cout << "running command" << std::endl;
-    // send the command
-    // wait for 'x' ms
-    Interface::send_data(this);
-}
-
 RESET * RESET::inst = nullptr;
 STOP * STOP::inst = nullptr;
+GET_HEALTH * GET_HEALTH::inst = nullptr;
+GET_INFO * GET_INFO::inst = nullptr;
+GET_SAMPLERATE * GET_SAMPLERATE::inst = nullptr;
+SCAN * SCAN::inst = nullptr;
+
+void SRNR::execute()
+{
+    Interface::send_data(this);
+    Interface::delay(2);
+}
+
+void SRSR::execute()
+{
+    Interface::send_data(this);
+    Interface::delay();
+    Interface::receive_data();
+}
+
+void SCAN::data_rx()
+{
+    // keep receiving the data and keep filling the buffer 
+}
+
+void SCAN::execute()
+{
+    Interface::send_data(this);
+    Interface::receive_data();
+
+    // if valid response is received
+    if (false)
+    {
+        receiver_thread = std::thread(data_rx);
+        m_scan_active = true;
+    }
+}
+
+SCAN::~SCAN()
+{
+    if (m_scan_active)
+        receiver_thread.join();
+}
+
+std::optional<int> SCAN::get_next_data()
+{
+    // TODO: read the buffer and return the element
+    return 0;
+}
--- lidar-interface/cmd.hpp
+++ lidar-interface/cmd.hpp
@@ -3,13 +3,23 @@
 #include <cstdint>
 #include <iostream>
 
+#include <optional>
+#include <thread>
+
+enum class STAT: int8_t
+{
+    SUCCESS = 0,
+    FAILED  = -1
+};
+
 enum class CMD_t: uint8_t
 {
     STOP           = 0x25, 
     RESET          = 0x40,
     GET_INFO       = 0x50,
     GET_HEALTH     = 0x52,
-    GET_SAMPLERATE = 0x59
+    GET_SAMPLERATE = 0x59,
+    SCAN           = 0x20
 };
 
 class CMD
@@ -66,4 +76,95 @@ public:
             inst = new RESET;
         return inst;
     }
+};
+
+class SRSR: public CMD
+{
+public:
+    SRSR(CMD_t id, uint8_t len): CMD(id, len)
+    {}
+
+    void execute();
+};
+
+class GET_INFO: public SRSR
+{
+    static GET_INFO * inst;
+
+public:
+    GET_INFO(CMD_t id = CMD_t::GET_INFO, uint8_t len = 2): SRSR(id, len)
+    {}
+
+    GET_INFO * getInst()
+    {
+        if(!inst) inst = new GET_INFO;
+        return inst;
+    }
+};
+
+class GET_HEALTH: public SRSR
+{
+    static GET_HEALTH * inst;
+public:
+    GET_HEALTH(CMD_t id = CMD_t::GET_HEALTH, uint8_t len = 2): SRSR(id, len)
+    {}
+
+    GET_HEALTH * getInst()
+    {
+        if (!inst) inst = new GET_HEALTH;
+        return inst;
+    }
+};
+
+class GET_SAMPLERATE: public SRSR
+{
+    static GET_SAMPLERATE * inst;
+public:
+    GET_SAMPLERATE(CMD_t id = CMD_t::GET_SAMPLERATE, uint8_t len = 2): SRSR(id, len)
+    {}
+
+    GET_SAMPLERATE * getInst()
+    {
+        if (!inst) inst = new GET_SAMPLERATE;
+        return inst;
+    }
+};
+
+class SRMR: public CMD
+{
+public:
+    SRMR(CMD_t id, uint8_t len): CMD(id, len)
+    {}
+};
+
+class SCAN: public SRMR
+{
+    static SCAN * inst;
+
+    int data[100]; // temp holder
+
+    std::thread receiver_thread;
+
+    bool m_scan_active;
+
+public:
+
+    SCAN(CMD_t id = CMD_t::SCAN, uint8_t len = 2): SRMR(id, len), 
+                                                   m_scan_active {false}
+    {}
+
+    SCAN * getInst()
+    {
+        if (!inst) inst = new SCAN;
+        return inst;
+    }
+
+    static void data_rx();
+
+    void execute();
+
+    std::optional<int> get_next_data();
+
+
+    ~SCAN();
 };
