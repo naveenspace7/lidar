ad5f467832470630a8979f6ea9d3e25cf325c93d
naveen chakravarthy <naveen.j_94@outlook.com>
2020-08-09 18:59:54 +0200

    Add C++17 support in the makefile

--- lidar-interface/main.cpp
+++ lidar-interface/main.cpp
@@ -3,13 +3,21 @@
 
 using namespace std;
 
-RESET * reset = reset->getInst();
-STOP * stop = stop->getInst();
+RESET * reset_cmd = reset_cmd->getInst();
+STOP * stop_cmd = stop_cmd->getInst();
+GET_INFO * get_info_cmd = get_info_cmd->getInst();
+GET_HEALTH * get_health_cmd = get_health_cmd->getInst();
+GET_SAMPLERATE * get_samplerate_cmd = get_samplerate_cmd->getInst();
+SCAN * scan_cmd = scan_cmd->getInst();
 
 int main()
 {
-    reset->execute();
-    stop->execute();
+    reset_cmd->execute();
+    stop_cmd->execute();
 
+    get_info_cmd->execute();
+    get_health_cmd->execute();
+    get_samplerate_cmd->execute();
 
+    scan_cmd->execute();
 }
