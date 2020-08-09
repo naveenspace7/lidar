2040986c638bf456d1262e80161db3080ce543a0
naveen chakravarthy <naveen.j_94@outlook.com>
2020-08-09 18:58:31 +0200

    Add functions to receive the data

--- lidar-interface/interface.cpp
+++ lidar-interface/interface.cpp
@@ -7,3 +7,14 @@ void Interface::send_data(const CMD * data)
     using namespace std;
     cout << "sending data from the interface " << hex << (int)data->ID << endl;
 }
+
+void Interface::delay(uint8_t wait_factor)
+{
+
+}
+
+void Interface::receive_data()
+{
+    using namespace std;
+    cout << "receiving data from the interface" << endl;
+}
--- lidar-interface/interface.hpp
+++ lidar-interface/interface.hpp
@@ -5,5 +5,8 @@
 class Interface
 {
 public:
+    static const uint8_t DURATION_MS = 1;
     static void send_data(const CMD * data);
+    static void delay(uint8_t wait_factor = 1);
+    static void receive_data();
 };
